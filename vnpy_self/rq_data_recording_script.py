8e6b33b3857f0eccccc98e66bf965021d33d5f44
EvanZhu2000 <ezvhaun@gmail.com>
2024-08-02 09:53:52 +0800

    optimize insert

--- vnpy_self/rq_data_recording_script.py
+++ vnpy_self/rq_data_recording_script.py
@@ -10,15 +10,17 @@ import rqdatac as rq
 from rqdatac import *
 rq.init('+85260983439','14261368Abc!')
 
-# Chinese futures market trading period (day/night)
+RECORD_INTERVAL = 5 # unit: minutes
+RECORD_FREQ = '1m'
+
 DAY_START = time(8, 45)
 DAY_END = time(15, 35)
 
+LUNCH_BREAK_START = time(11, 30 + RECORD_INTERVAL + 1)
+LUNCH_BREAK_END = time(13, 0)
+
 NIGHT_START = time(20, 45)
 NIGHT_END = time(2, 45)
-
-RECORD_INTERVAL = 5 # unit: minutes
-RECORD_FREQ = '1m'
 
 
 def check_trading_period():
@@ -34,6 +36,11 @@ def check_trading_period():
         trading = True
 
     return trading
+
+
+def is_lunch_break():
+    current_time = datetime.now().time()
+    return current_time >= LUNCH_BREAK_START and current_time <= LUNCH_BREAK_END
 
 
 
@@ -55,18 +62,19 @@ def run():
             aaa['interval'] = RECORD_FREQ
             aaa = aaa[['symbol', 'exchange', 'datetime', 'interval', 'volume', 'turnover', 'open_interest', 'open_price', 'high_price', 'low_price', 'close_price']]
             sqlservice.insert_datafeed(aaa, ignore=True)
-            
+
             sqlservice.close()
         else:
-            info = get_price(data['order_book_id'].loc[(data['order_book_id'].str.contains('99') == False) &
-                    (data['order_book_id'].str.contains('88') == False)].dropna().unique().tolist(), 
-                    start_date = tdy_str, 
-                    end_date   = tdy_str, 
-                    frequency=RECORD_FREQ)
-            aaa = info.swaplevel().sort_index().loc[info.swaplevel().sort_index().index.levels[0][-(RECORD_INTERVAL):]].reset_index().set_index('order_book_id').join(data[['order_book_id', 'exchange','trading_code']].set_index('order_book_id')).reset_index().drop(['order_book_id'],axis=1).rename(columns={"total_turnover": "turnover", "open":"open_price","low":"low_price","high":"high_price","close":"close_price","trading_code":"symbol"})
-            aaa['interval'] = RECORD_FREQ
-            aaa = aaa[['symbol', 'exchange', 'datetime', 'interval', 'volume', 'turnover', 'open_interest', 'open_price', 'high_price', 'low_price', 'close_price']]
-            sqlservice.insert_datafeed(aaa)
+            if not is_lunch_break():
+                info = get_price(data['order_book_id'].loc[(data['order_book_id'].str.contains('99') == False) &
+                        (data['order_book_id'].str.contains('88') == False)].dropna().unique().tolist(), 
+                        start_date = tdy_str, 
+                        end_date   = tdy_str, 
+                        frequency=RECORD_FREQ)
+                aaa = info.swaplevel().sort_index().loc[info.swaplevel().sort_index().index.levels[0][-(RECORD_INTERVAL-1):]].reset_index().set_index('order_book_id').join(data[['order_book_id', 'exchange','trading_code']].set_index('order_book_id')).reset_index().drop(['order_book_id'],axis=1).rename(columns={"total_turnover": "turnover", "open":"open_price","low":"low_price","high":"high_price","close":"close_price","trading_code":"symbol"})
+                aaa['interval'] = RECORD_FREQ
+                aaa = aaa[['symbol', 'exchange', 'datetime', 'interval', 'volume', 'turnover', 'open_interest', 'open_price', 'high_price', 'low_price', 'close_price']]
+                sqlservice.insert_datafeed(aaa, ignore=True)
 
         sleep(60*RECORD_INTERVAL)
 
