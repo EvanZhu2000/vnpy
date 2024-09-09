4946bc867c33db91367a5459b146e0996e25973b
EvanZhu2000 <ezvhaun@gmail.com>
2024-09-09 16:40:33 +0800

    fix bug

--- vnpy_self/strategy/strategy2_schedule_updater.py
+++ vnpy_self/strategy/strategy2_schedule_updater.py
@@ -9,10 +9,10 @@ today_date = datetime.today()
 if __name__ == "__main__":    
     set1 = {'IF','IH','IC','IM','T','TS','TF','TL'}
     set2 = {'AG','AU','SC','EC','CU'}
-    set3 = {}  # self definition
+    set3 = set({}) # self definition
     to_drop_list = list(set1|set2|set3)
     df = mysqlservice.select('trading_schedule', 'order by date desc', strateg='dom').iloc[0]
-    potential_trading_list = (pd.Series(df['symbol'].split(',')).str[:-4]).tolist()
+    potential_trading_list = (pd.Series(df['symbol'].split(',')).str.split('.').str[0].str[:-4]).tolist()
     trading_list = list(set(potential_trading_list) - set(to_drop_list))
-    mysqlservice.insert('trading_schedule',date = df['date'], symbol = ','.join(trading_list), strategy='strategy2', sc_symbol='strategy2')
+    mysqlservice.insert('trading_schedule',date = df['date'], symbol = ','.join(trading_list), strategy='strategy2', sc_symbol='dom')
     mysqlservice.close()
