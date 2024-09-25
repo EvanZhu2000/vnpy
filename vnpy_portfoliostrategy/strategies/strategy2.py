b6c5c215d16687035361071b085ffaa82b5f02bf
EvanZhu2000 <ezvhaun@gmail.com>
2024-09-25 10:00:20 +0800

    loggings

--- vnpy_portfoliostrategy/strategies/strategy2.py
+++ vnpy_portfoliostrategy/strategies/strategy2.py
@@ -44,6 +44,7 @@ class Strategy2(StrategyTemplate):
     def on_tick(self, tick: TickData) -> None:
         """行情推送回调"""
         if self.bool_dict.all_true():
+            self.write_log("All have rebalanced. Stop the strategy now")
             self.strategy_engine.stop_strategy(self.strategy_name)
             return
         
