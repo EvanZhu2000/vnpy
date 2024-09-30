479aedf09e973269eed4f80f0a52d6aae316e269
EvanZhu2000 <ezvhaun@gmail.com>
2024-09-30 14:48:15 +0800

    add full daily recoding process

--- vnpy_self/data_and_db/nth_rq_data_filler.sh
+++ vnpy_self/data_and_db/nth_rq_data_filler.sh
@@ -3,4 +3,4 @@
 # Assign the current date to date_str
 date_str=$(date +%Y%m%d)
 
-/home/evan/miniconda3/envs/vnpy3/bin/python /home/evan/miniconda3/envs/vnpy3/lib/python3.10/site-packages/vnpy_self/data_and_db/nth_rq_data_filler.py "dominant" $date_str $date_str
+/home/evan/miniconda3/envs/vnpy3/bin/python /home/evan/miniconda3/envs/vnpy3/lib/python3.10/site-packages/vnpy_self/data_and_db/nth_rq_data_filler.py "dominant,member_rank,price" $date_str $date_str
