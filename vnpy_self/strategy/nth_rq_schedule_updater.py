c755a364b5fa4b668dda62af3cea5d15c922252e
EvanZhu2000 <ezvhaun@gmail.com>
2024-10-23 10:46:19 +0800

    fix trading hour insertion

--- vnpy_self/strategy/nth_rq_schedule_updater.py
+++ vnpy_self/strategy/nth_rq_schedule_updater.py
@@ -24,18 +24,25 @@ if __name__ == "__main__":
 
     today_str = datetime.today().strftime('%Y-%m-%d')
     next_trading_day = get_next_trading_date(today_str).strftime('%Y-%m-%d')
+    rq_today_dom_list,rq_today_dom2_list = [],[]
     rq_next_trading_day_dom_list,rq_next_trading_day_dom2_list = [],[]
     
     for i in all_futures_df['underlying_symbol'].values:
         dom_contract_next_day = futures.get_dominant(i,next_trading_day,rule=0,rank=1).values[0]
         rq_next_trading_day_dom_list.append(dom_contract_next_day)
+        dom_contract_today = futures.get_dominant(i,today_str,rule=0,rank=1).values[0]
+        rq_today_dom_list.append(dom_contract_today)
         
         if i in symb_list:
             dom2_contract_next_day = futures.get_dominant(i,next_trading_day,rule=0,rank=2).values[0]
             rq_next_trading_day_dom2_list.append(dom2_contract_next_day)
+            dom2_contract_today = futures.get_dominant(i,today_str,rule=0,rank=2).values[0]
+            rq_today_dom2_list.append(dom2_contract_today)
 
     next_trading_day_dom_list = symbol_rq2vnpy(rq_next_trading_day_dom_list)
     next_trading_day_dom2_list = symbol_rq2vnpy(rq_next_trading_day_dom2_list)
+    today_dom_list =  symbol_rq2vnpy(rq_today_dom_list)
+    today_dom2_list = symbol_rq2vnpy(rq_today_dom2_list)
     mysqlservice.insert('trading_schedule', ignore=True, today = today_str, date = next_trading_day, symbol = ','.join(rq_next_trading_day_dom_list), strategy = 'dom', sc_symbol = 'rq_dom')
     mysqlservice.insert('trading_schedule', ignore=True, today = today_str, date = next_trading_day, symbol = ','.join(rq_next_trading_day_dom2_list), strategy = 'dom2', sc_symbol = 'rq_dom2')
     
@@ -49,6 +56,20 @@ if __name__ == "__main__":
     for i in range(len(next_trading_day_dom2_list)):
         symb = next_trading_day_dom2_list[i]
         rq_symb = rq_next_trading_day_dom2_list[i]
+        mysqlservice.insert('trading_hours', ignore=True, date = next_trading_day, 
+                            rqsymbol = rq_symb, symbol = symb, 
+                            trading_hours = get_trading_hours(rq_symb, next_trading_day), timezone = 'Asia/Shanghai')
+        
+    for i in range(len(today_dom_list)):
+        symb = today_dom_list[i]
+        rq_symb = rq_today_dom_list[i]
+        mysqlservice.insert('trading_hours', ignore=True, date = next_trading_day, 
+                            rqsymbol = rq_symb, symbol = symb, 
+                            trading_hours = get_trading_hours(rq_symb, next_trading_day), timezone = 'Asia/Shanghai')
+        
+    for i in range(len(today_dom2_list)):
+        symb = today_dom2_list[i]
+        rq_symb = rq_today_dom2_list[i]
         mysqlservice.insert('trading_hours', ignore=True, date = next_trading_day, 
                             rqsymbol = rq_symb, symbol = symb, 
                             trading_hours = get_trading_hours(rq_symb, next_trading_day), timezone = 'Asia/Shanghai')
