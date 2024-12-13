3ae42cdabf3068300c46481a20ae2a2d1a218cdd
EvanZhu2000 <ezvhaun@gmail.com>
2024-12-13 11:38:45 +0800

    need to change this in the future!

--- vnpy_self/data_and_db/db_setting.py
+++ vnpy_self/data_and_db/db_setting.py
@@ -1,11 +1,11 @@
-# db_setting = {
-#     'host':"192.168.91.124",
-#     'user': "evan",
-#     'password': "Evan@cash1q2"
-# }
-
 db_setting = {
-    'host':"192.168.91.121",
+    'host':"192.168.91.124",
     'user': "evan",
-    'password': "evan@cash1q2"
+    'password': "Evan@cash1q2"
 }
+
+# db_setting = {
+#     'host':"192.168.91.121",
+#     'user': "evan",
+#     'password': "evan@cash1q2"
+# }
